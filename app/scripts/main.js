36375f394a0b343c751d1c8ca63ad4f97b4d6f5a
danieliniguezv <48453683+danieliniguezv@users.noreply.github.com>
2023-05-24 15:48:25 -0600

    Added futures contract and FUSDC balance display

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,5 @@
 const web3 = new Web3("https://filecoin-hyperspace.chainstacklabs.com/rpc/v1");
-const abi = [
+const futuresContractAbi = [
 	{
 		"inputs": [],
 		"name": "getSpecs",
@@ -175,8 +175,420 @@ const abi = [
 		"type": "function"
 	}];
 
-const contractAddress = "0x5B1cB45B5C20a68660315F9AC13D326C93C66964";
-const contract = new web3.eth.Contract(abi, contractAddress);
+const futuresContractIssuerAbi = [
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_futuresContractAddress",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_amount",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"inputs": [],
+		"name": "futuresContractAddress",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_owner",
+				"type": "address"
+			}
+		],
+		"name": "getBalance",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getSpecs",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "productCode",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "settlementCoin",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "quoteCoin",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "expiryDate",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "fileFormat",
+				"type": "string"
+			},
+			{
+				"internalType": "uint256",
+				"name": "contractSize",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "minimumPriceIncrement",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "leverage",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "initialMargin",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "maintenanceMargin",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_owner",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_amount",
+				"type": "uint256"
+			}
+		],
+		"name": "transferFuturesContracts",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_from",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "_to",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_amount",
+				"type": "uint256"
+			}
+		],
+		"name": "transferFuturesContractsFrom",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	}
+];
+
+const filecoinUSDCAbi = [
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "owner",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "spender",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "value",
+				"type": "uint256"
+			}
+		],
+		"name": "Approval",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "value",
+				"type": "uint256"
+			}
+		],
+		"name": "Transfer",
+		"type": "event"
+	},
+	{
+		"inputs": [],
+		"name": "_totalSupply",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_owner",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "_spender",
+				"type": "address"
+			}
+		],
+		"name": "allowance",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_spender",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_amount",
+				"type": "uint256"
+			}
+		],
+		"name": "approve",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_account",
+				"type": "address"
+			}
+		],
+		"name": "balanceOf",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "decimals",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "name",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "owner",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "symbol",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "totalSupply",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_to",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_amount",
+				"type": "uint256"
+			}
+		],
+		"name": "transfer",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_from",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "_to",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_amount",
+				"type": "uint256"
+			}
+		],
+		"name": "transferFrom",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	}
+];
+
+const filecoinUSDCContractAddress = "0x8A0C0417434a382604F0DD0aF3146dB6944331B7";
+const filecoinUSDCContract = new web3.eth.Contract(filecoinUSDCAbi, filecoinUSDCContractAddress);
+
+const futuresContractContractAddress = "0x5B1cB45B5C20a68660315F9AC13D326C93C66964";
+const futuresContractContract = new web3.eth.Contract(futuresContractAbi, futuresContractContractAddress);
+
+const futuresContractIssuerContractAddress = "0x6e823E1dF0Ae4f6eCad9734F88D96469A3A9BBDf";
+const futuresContractIssuerContract = new web3.eth.Contract(futuresContractIssuerAbi, futuresContractIssuerContractAddress);
 
 async function connect() {
 	if (window.ethereum) {
@@ -192,9 +604,14 @@ async function connect() {
 			let _balance = web3.utils.fromWei(balance, "ether");
 			document.getElementById("balance").innerHTML = _balance;
 		});
-    
+
+    let filecoinUSDCBalance;
+    filecoinUSDCBalance = await filecoinUSDCContract.methods.balanceOf(walletAddress).call();
+    document.getElementById("filecoin-usdc-balance").innerHTML = filecoinUSDCBalance;
+    console.log(filecoinUSDCBalance);
+
     let specs = [];
-    specs = await contract.methods.getSpecs().call();
+    specs = await futuresContractContract.methods.getSpecs().call();
     document.getElementById("product-code").innerHTML = specs[0];
     console.log(specs[0]);
     document.getElementById("settlement-coin").innerHTML = specs[1];
@@ -215,6 +632,11 @@ async function connect() {
     console.log(specs[8]);
     document.getElementById("maintenance-margin").innerHTML = specs[9];
     console.log(specs[9]);
+
+    let futuresContractBalance;
+    futuresContractBalance = await futuresContractIssuerContract.methods.getBalance(walletAddress).call();
+    document.getElementById("futures-contract-balance").innerHTML = futuresContractBalance;
+    console.log(futuresContractBalance);
 	} else {
 		console.log("No Wallet");
 	}
