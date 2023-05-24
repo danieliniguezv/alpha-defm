d103960caddfcad169db8c3fc61a26fdaa37b965
danieliniguezv <48453683+danieliniguezv@users.noreply.github.com>
2023-05-24 15:17:24 -0600

    Library paths update

--- src/FuturesExchange.sol
+++ src/FuturesExchange.sol
@@ -1,9 +1,9 @@
 //SPDX-License-Identifier: UNLICENSED
 
-pragma solidity 0.8.20;
+pragma solidity ^0.8.0;
 
-import "contracts/FuturesContractIssuer.sol";
-import "contracts/FilecoinUSDC.sol";
+import "./FuturesContractIssuer.sol";
+import "./FilecoinUSDC.sol";
 
 
 contract FuturesExchange {
